db30093cc82234f1cf45d832ff1ed7a19c3468c6
Shrikant Satle <101779959+shrisat@users.noreply.github.com>
2026-03-20 15:54:41 +0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -40,7 +40,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.crm_cust_info ';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -57,7 +57,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.crm_prd_info ';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH(
 		FIRSTROW = 2,
 		FIELDTERMINATOR =',',
@@ -73,7 +73,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.crm_sales_details ';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH(
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -93,7 +93,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.erp_cust_az12 ';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH(
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
@@ -109,7 +109,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.erp_loc_a101 ';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH(
 		FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -125,7 +125,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into:bronze.erp_px_cat_glv2 ';
 		BULK INSERT bronze.erp_px_cat_glv2
-		FROM 'C:\Users\Azendo User\Downloads\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'C:\SQL DATAWAREHOUSE PROJECT\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH(
 		FIRSTROW = 2,
 		FIELDTERMINATOR = ',',
